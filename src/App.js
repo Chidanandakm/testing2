fa6f3e9145eb39794d8ed90cbf91093513ff8bc9
chidananda <chidananda@iterator.in>
2022-11-23 12:33:30 +0530

    changed largely

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       fourth change
+      {data?.total_rise_prop?.filter(i => i.prop_value?.includes('LOW'))?.map((item) => (
+                <div style={{ minWidth: '300px' }}>
+                  <Card title={capitalize(item.prop_value)} sectioned>
+                    <DisplayText size='large' >{item.total}</DisplayText>
+                    <TextStyle>{(item.alteration_ratio * 100).toFixed(2)}%</TextStyle>
+                  </Card>
+                </div>
+              ))}
+              <div style={{ minWidth: '300px' }}>
+                <Card title={'Mid'} sectioned>
+                  <DisplayText size='large' s>{data?.total_rise_prop?.filter(i => i.prop_value?.includes('MID'))?.map(i => i.total)?.reduce((pr, cu) => pr + cu, 0)}</DisplayText>
+                  <TextStyle>{data?.total_rise_prop?.filter(i => i.prop_value?.includes('MID'))?.map(i => i.alteration_ratio)?.reduce((pr, cu) => pr + cu, 0).toFixed(2)}%</TextStyle>
+                </Card>
+              </div>
+              <div style={{ minWidth: '300px' }}>
+                <Card title={'High'} sectioned>
+                  <DisplayText size='large' s>{data?.total_rise_prop?.filter(i => i.prop_value?.includes('HIGH'))?.map(i => i.total)?.reduce((pr, cu) => pr + cu, 0)}</DisplayText>
+                  <TextStyle>{data?.total_rise_prop?.filter(i => i.prop_value?.includes('HIGH'))?.map(i => i.alteration_ratio)?.reduce((pr, cu) => pr + cu, 0).toFixed(2)}%</TextStyle>
+                </Card>
+              </div>
     </div>
   );
 }
